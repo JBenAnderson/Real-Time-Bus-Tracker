f9e9843b52baf91b68cebec2c04c06ce9ed5c3c8
J Ben Anderson <revovisions@gmail.com>
2021-07-30 15:22:31 -0400

    fixed map window for fluid view

--- mapanimation.js
+++ mapanimation.js
@@ -1,27 +1,35 @@
-mapboxgl.accessToken = "INSERT ACCESS TOKEN";
+mapboxgl.accessToken =
+  "pk.eyJ1IjoicmV2b3Zpc2lvbnMiLCJhIjoiY2tycG5rbzF4OHEwZTMxbzg1Nnpkd3R5ZSJ9.6ELY0c9jpUyT21w8qSv1qg";
 const map = new mapboxgl.Map({
   container: "map", // container ID
   style: "mapbox://styles/revovisions/ckm2hlo8e0vlt18qtn53ulawp", // style URL
   center: [-71.104081, 42.365554], // starting position [lng, lat]
-  zoom: 17, // starting zoom
+  zoom: 12, // starting zoom
   pitch: 20,
 });
 
 async function run() {
   // get bus data
+
   const locations = await getBusLocations();
 
   for (let i = 0; i < locations.length; i++) {
-    const marker = new mapboxgl.Marker();
-    const longitude = locations[i].attributes.longitude;
-    const latitude = locations[i].attributes.latitude;
-    marker.setLngLat([longitude, latitude]);
-    marker.addTo(map);
-    marker.setPopup(new mapboxgl.Popup().setHTML(locations[i].id));
+    if (locations.length !== undefined) {
+      const marker = new mapboxgl.Marker();
+      const longitude = locations[i].attributes.longitude;
+      const latitude = locations[i].attributes.latitude;
+      marker.setLngLat([longitude, latitude]);
+      marker.addTo(map);
+      marker.setPopup(new mapboxgl.Popup().setHTML(locations[i].id));
+    } else {
+      setTimeout(() => marker.remove(), 15000);
+    }
   }
 }
 
 setTimeout(() => {
+  getBusLocations();
+
   run();
 }, 15000);
 
